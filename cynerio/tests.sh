c6a5effe5d824fac2918c93e2d40c8bbd114c5ea
Saar Wintrov <saar@rele.ai>
2021-11-24 16:12:06 +0200

    Added readme

---
+++ cynerio/tests.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+# you should build a basic devops hello wold, so to speak
+# by the end you should have:
+# a docker-compose.yaml file - should deploy the api + redis
+# a python http service file
+# a dockerfile to build it
+# and this test file to test it
+
+set -x
+
+# Add an api `/healthcheck` which will always return {"status":"ok"}
+
+# Test `/healthcheck` call -- should return
+output=$(curl \
+    --write-out '%{http_code}' \
+    --silent \
+    --output /dev/null \
+    http://localhost:5000/healthcheck)
+if [[ $output != '200' ]]; then
+    echo "ERROR: healthcheck request failed with response code $output"
+    exit 1
+fi
+
+output=$(curl \
+    --fail \
+    --silent \
+    http://localhost:5000/healthcheck)
+if [[ $output != '{"status":"ok"}' ]]; then
+    echo "ERROR: healthcheck request got unexpected output: $output"
+    exit 1
+fi
+
+# Add an api `/get/<value-id>` which will try to get key named `<value-id>` from redis
+
+# test `/get/something` with a non-existing value - should return 404
+output=$(curl \
+    --write-out '%{http_code}' \
+    --silent \
+    --output /dev/null \
+    http://localhost:5000/get/something)
+if [[ $output != '404' ]]; then
+    echo "ERROR: /get/something request did not return 404. Got instead $output"
+    exit 1
+fi
+
+# Add an api `/set` which will allow posting json body of the form {"<id>": "<data>"}
+# which will then be posted to redis with key `<id>` and data `<data>`
+
+# test `/set` with data '{"id": "one", "data": "one"}'. Should return 2xx.
+output=$(curl \
+    --write-out '%{http_code}' \
+    --silent \
+    --output /dev/null \
+    --request POST \
+    --data '{"id": "one", "data": "one"}' \
+    http://localhost:5000/set)
+if [[ $output =~ '2\d\d' ]]; then
+    echo "ERROR: /set request did not return 2xx. Got instead $output"
+    exit 1
+fi
+
+# test `/get/one` with a non-existing value - should return 200 and the value
+output=$(curl \
+    --write-out '%{http_code}' \
+    --silent \
+    --output /dev/null \
+    http://localhost:5000/get/one)
+if [[ $output != '200' ]]; then
+    echo "ERROR: /get/one request did not return 200. Got instead $output"
+    exit 1
+fi
+
+output=$(curl \
+    --fail \
+    --silent \
+    http://localhost:5000/get/one)
+if [[ $output != '{"one":"one"}' ]]; then
+    echo "ERROR: /get/one request got unexpected output: $output"
+    exit 1
+fi
+
+
+echo "COMPLETED SUCCESSFULLY"
