f6b5c442d0ef40ffefd8c3309e07272a581c6824
Saar Wintrov <saar@rele.ai>
2021-11-24 14:24:35 +0200

    Fix the app

--- cynerio/api/v1/main.py
+++ cynerio/api/v1/main.py
@@ -1,8 +1,9 @@
-import redis
 import json, os
 from flask import Flask, request, jsonify
+from package.utils import *
 
 app = Flask(__name__)
+_redis = Redis_actions(os.environ["REDIS_HOST"])
 
 @app.route("/set", methods=["POST"] )
 def write_to_redis():
@@ -10,13 +11,11 @@ def write_to_redis():
     write to redis and returns 200
     '''
     obj = json.loads(request.get_data().decode('utf-8'))
-    r = redis_init()
-    try:
-        r.set(obj["id"], obj["data"])
-        return jsonify({"message": "Success"}), 200
-    except Exception as e:
-        print(e)
-        return jsonify({ "message": str(e) }), 500
+    ans = _redis.set_redis(obj["id"], obj["data"])
+    if ans == True:
+        return jsonify({ "message": "Success" }), 200
+    else:
+        return jsonify({ "message": "Something went wrong" }), 500
 
 @app.route("/healthcheck", methods=["GET"] )
 def healthz():
@@ -30,19 +29,10 @@ def get_data(_id):
     '''
     get value from redis
     '''
-    r = redis_init()
     try:
-        from_redis = r.get(_id).decode('utf-8')
-        return jsonify({_id: from_redis})
-    except Exception as e:
-        return jsonify({ "message": str(e) }), 500
-
-def redis_init():
-    '''
-    init redis
-    '''
-    r = redis.Redis(host=os.environ["REDIS_HOST"], port=6379, db=0)
-    return r
+        return jsonify({ _id: _redis.get_redis(_id) })
+    except Exception:
+        return jsonify({ "message": "Something went wrong" }), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
