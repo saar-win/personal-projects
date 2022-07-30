3cf7871e12b6de22d3fbba3fd1d8d9baf1ef50dd
wintrov <saar@rapidapi.com>
2022-07-31 00:57:30 +0300

    test

--- settings/setup.sh
+++ settings/setup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 echo $INPUT_GITHUB_LOGIN
+echo $INPUT_ACTIONS_ACCESS_USERNAME
+echo $INPUT_ACTIONS_ACCESS_KEY
 
-if [[ $3 == "true" ]]; then
-    git config --global url."https://$ACTIONS_ACCESS_USERNAME:$ACTIONS_ACCESS_KEY@github.com/".insteadOf "https://github.com/"
+if [[ $INPUT_GITHUB_LOGIN == "true" ]]; then
+    git config --global url."https://$INPUT_ACTIONS_ACCESS_USERNAME:$INPUT_ACTIONS_ACCESS_KEY@github.com/".insteadOf "https://github.com/"
     echo "Github login is enabled"
 else
     echo "Github login is disabled"
