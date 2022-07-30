645290426cb5ec4f8144c58505fe128b90b87390
wintrov <saar@rapidapi.com>
2022-07-31 01:30:05 +0300

    test

--- new_action/main.py
+++ new_action/main.py
@@ -1,7 +1,9 @@
+from email import header
 import os
 import yaml
 import requests
 import subprocess
+import uuid
 
 def main():
     '''
@@ -9,7 +11,7 @@ def main():
     yaml_file = load_yaml(os.getenv('INPUT_FILE'))
     print(yaml_file)
     git_actions()
-    open_git_pr(github_repository="saar-win/personal-projects")
+    open_git_pr()
 
 def load_yaml(file_path):
     '''
@@ -27,17 +29,21 @@ def git_actions():
     subprocess.run("git push --set-upstream origin test", shell=True)
     # return
 
-def open_git_pr(github_repository):
+def open_git_pr():
     '''
     '''
     service_name = "test"
-    res = requests.post('https://api.github.com/repos/{}/pulls'.format(github_repository),
+    headers = {
+        "Accept": "application/vnd.github.v3+json"
+    }
+    res = requests.post('https://api.github.com/repos/saar-win/personal-projects/pulls',
         json={
             'title': 'New Action',
             'body': f'New service {service_name}',
-            'head': 'test',
+            'head': f'test_{uuid.uuid4()}',
             'base': 'master'
-        }
+        },
+    headers=headers
     )
     print(res.text)
     if res is not None:
