cbf2df769a847019c97112b65c308b547b801f8d
wintrov <saar@rapidapi.com>
2022-07-31 00:40:14 +0300

    test

--- settings/setup.sh
+++ settings/setup.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-echo $1
-echo $GITHUB_LOGIN
+echo $3
 
-if [[ $1 == "true" ]]; then
+if [[ $3 == "true" ]]; then
     echo "Github login is enabled"
     git config --global url."https://$ACTIONS_ACCESS_USERNAME:$ACTIONS_ACCESS_KEY@github.com/".insteadOf "https://github.com/"
 else
