eab59d39cc3a8a0e370c627e18d38c50a8d8d80a
wintrov <saar@rapidapi.com>
2022-07-30 19:43:00 +0300

    test

--- new_action/main.py
+++ new_action/main.py
@@ -1,19 +1,13 @@
-from asyncio import subprocess
-import os
-import subprocess
-from datetime import datetime
+import yaml
 import sys
 
 def main():
     '''
     '''
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    subprocess.run(f'echo "::set-output name=time::{current_time}"', shell=True)
-    subprocess.run(f'ls -l', shell=True)
     _file = sys.argv[1]
-    with open(_file, 'r') as f:
-        print(f.read())
+    yaml_file = yaml.safe_load(open(_file))
+    print(yaml_file)
+
 
 if __name__ == '__main__':
     main()
