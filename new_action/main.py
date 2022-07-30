a3bd9738f409aaa56e2ffb74dcf79596274dd8a5
wintrov <saar@rapidapi.com>
2022-07-30 19:01:15 +0300

    test

--- new_action/main.py
+++ new_action/main.py
@@ -7,6 +7,7 @@ import sys
 def main():
     '''
     '''
+    print(os.environ)
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     subprocess.run(f'echo "::set-output name=time::{current_time}"', shell=True)
