13db677880631107102df93706074dcc14083e59
wintrov <saar@rapidapi.com>
2022-07-31 00:59:04 +0300

    test

--- new_action/main.py
+++ new_action/main.py
@@ -1,10 +1,11 @@
 import yaml
 import sys
+import os
 
 def main():
     '''
     '''
-    _file = sys.argv[1]
+    _file = os.getenv('INPUT_FILE')
     yaml_file = yaml.safe_load(open(_file))
     print(yaml_file)
 
--- settings/setup.sh
+++ settings/setup.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-echo $INPUT_GITHUB_LOGIN
-echo $INPUT_ACTIONS_ACCESS_USERNAME
-echo $INPUT_ACTIONS_ACCESS_KEY
 
 if [[ $INPUT_GITHUB_LOGIN == "true" ]]; then
     git config --global url."https://$INPUT_ACTIONS_ACCESS_USERNAME:$INPUT_ACTIONS_ACCESS_KEY@github.com/".insteadOf "https://github.com/"
