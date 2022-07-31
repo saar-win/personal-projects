34fdcee10e6b15b413fbd100e29082a40dcd2451
wintrov <saar@rapidapi.com>
2022-07-31 21:17:03 +0300

    test

--- new_action/main.py
+++ new_action/main.py
@@ -161,13 +161,14 @@ def git_actions(branch_name):
     '''
     '''
     commit_msg = "test"
-    subprocess.run("sudo chown -R runner .", shell=True, capture_output=True, text=True).stdout
-    subprocess.run(f"git config --global user.email 'saar1122@gmail.com'", shell=True, capture_output=True, text=True).stdout
-    subprocess.run(f"git config --global user.name 'saar-win'", shell=True, capture_output=True, text=True).stdout
-    subprocess.run(f"git checkout -b {branch_name}", shell=True)
-    subprocess.run("git add -A", shell=True)
-    subprocess.run(f"git commit -am {commit_msg}", shell=True)
-    subprocess.run(f"git push --set-upstream origin {branch_name}", shell=True)
+    x = subprocess.run('sudo chown -R runner .', shell=True, capture_output=True, text=True).stdout
+    print(x)
+    subprocess.run(f'git config --global user.email "saar1122@gmail.com"', shell=True, capture_output=True, text=True).stdout
+    subprocess.run(f'git config --global user.name "saar-win"', shell=True, capture_output=True, text=True).stdout
+    subprocess.run(f'git checkout -b {branch_name}', shell=True)
+    subprocess.run('git add -A', shell=True)
+    subprocess.run(f'git commit -am {commit_msg}', shell=True)
+    subprocess.run(f'git push --set-upstream origin {branch_name}', shell=True)
     # return
 
 def open_git_pr(branch_name, service_name, repo_name):
