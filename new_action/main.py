d254d5abdf20e3823a16e1c1c4b0a8b636598778
wintrov <saar@rapidapi.com>
2022-07-31 21:14:50 +0300

    test

--- new_action/main.py
+++ new_action/main.py
@@ -164,7 +164,6 @@ def git_actions(branch_name):
     subprocess.run("sudo chown -R runner .", shell=True, capture_output=True, text=True).stdout
     subprocess.run(f"git config --global user.email 'saar1122@gmail.com'", shell=True, capture_output=True, text=True).stdout
     subprocess.run(f"git config --global user.name 'saar-win'", shell=True, capture_output=True, text=True).stdout
-    print(x)
     subprocess.run(f"git checkout -b {branch_name}", shell=True)
     subprocess.run("git add -A", shell=True)
     subprocess.run(f"git commit -am {commit_msg}", shell=True)
